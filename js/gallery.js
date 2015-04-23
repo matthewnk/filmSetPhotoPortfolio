ddf465f53a1ebfba7e89a67a4a7d5f94866957bc
Matthew King <mrmatthew.nk@gmail.com>
2015-04-22 17:43:32 -0700

    Added Google Analytics, Mailchip form, contact info.

--- js/gallery.js
+++ js/gallery.js
@@ -57,7 +57,22 @@ $(document).on('click', 'a.controls', function(){
     }else{
         $('a.previous').show()
     }
-    
-    
+        
     return false;
 })
+
+// javascript for mailchip form
+
+// function($){
+//     window.fnames = new Array(); 
+//     window.ftypes = new Array();
+//     fnames[0]='EMAIL';
+//     ftypes[0]='email';
+//     fnames[1]='FNAME';
+//     ftypes[1]='text';
+//     fnames[2]='LNAME';
+//     ftypes[2]='text';
+// }
+
+// (jQuery));
+// var $mcj = jQuery.noConflict(true);
